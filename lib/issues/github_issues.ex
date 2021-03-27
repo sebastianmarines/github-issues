d1aa0aa8f26ddc205813ef10238358748359e72a
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-27 14:00:05 -0600

    Parse response body with poison

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -11,12 +11,13 @@ defmodule Issues.GithubIssues do
     "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}) do
-    {:ok, body}
+  def handle_response({:ok, %{status_code: status_code, body: body}}) do
+    {
+      check_for_error(status_code),
+      Poison.Parser.parse!(body)
+    }
   end
 
-  def handle_response({_, %{status_code: _, body: body}}) do
-    {:error, body}
-  end
-
+  def check_for_error(200), do: :ok
+  def check_for_error(_), do: :error
 end
